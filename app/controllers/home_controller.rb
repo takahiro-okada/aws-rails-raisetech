59e31fd46db1b6d3ba09bb28325343ede9a9c0cb
takahiro okada <t.okada@koshikiad.com>
2021-11-03 21:37:51 +0900

    update: add edit function

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @title = 'AWSクラス'
-  end
-
-  def about
-  end
-end
---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,34 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo= Todo.new
+  end
+
+  def create
+    @todo= Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :content)
+    end
+end
+
